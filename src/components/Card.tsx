be6040f00d703a1e1592c2d8b2542a31fa141a46
Victor Araújo <victor.b_@hotmail.com>
2023-09-20 14:50:42 +0000

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CardType } from "../containers/Cards/types";
 
 interface CardProps {
@@ -5,10 +6,39 @@ interface CardProps {
 }
 
 export const Card = ({ card }: CardProps) => {
+  const [showFull, setShowFull] = useState(false);
   const img = card.card_images[0].image_url;
   return (
     <div className="p-2 w-1/4">
-      <img src={img} alt={card.name} title={card.name} />
+      <img
+        src={img}
+        alt={card.name}
+        title={card.name}
+        onClick={() => setShowFull(true)}
+      />
+      {showFull && (
+        <div
+          className="h-screen w-screen flex-col
+            bg-gradient-to-l from-black flex items-center justify-center
+            absolute top-0 left-0 transition-opacity duration-700
+        "
+        >
+          <button
+            className="text-xl px-2 m-2 bg-red-700 
+            ml-[20%]
+            rounded-full text-gray-100 hover:bg-gray-500 "
+            onClick={() => setShowFull(false)}
+          >
+            X
+          </button>
+          <img
+            src={img}
+            alt={card.name}
+            title={card.name}
+            onClick={() => setShowFull(true)}
+          />
+        </div>
+      )}
     </div>
   );
 };
