41772349677ad21f08b987c3b696b829999f1733
Victor Araújo <victor.b_@hotmail.com>
2023-09-28 17:01:13 +0000

    Update Main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,10 +2,16 @@ import { Sidebar } from "../containers/Sidebar/Sidebar";
 import { Cards } from "../containers/Cards/Cards";
 
 export const Main = () => {
- return (
-  <div className="flex">
-    <Sidebar />
-    <Cards />
-  </div>
- )
-}
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-4xl m-4 text-rose-500">
+        YugiTop
+        <span className="text-amber-500">.</span>
+      </h1>
+      <div className="flex">
+        <Sidebar />
+        <Cards />
+      </div>
+    </div>
+  );
+};
